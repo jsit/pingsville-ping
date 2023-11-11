0ed0eb6cbcb2e3ebfb812d02d388b1510394a5e9
Jay Sitter <jay@jaysitter.com>
2023-11-11 13:22:16 -0500

    fix: Use Deno.env.get for env vars

--- utils/db/client.ts
+++ utils/db/client.ts
@@ -2,20 +2,20 @@ import { load } from 'https://deno.land/std@0.206.0/dotenv/mod.ts';
 import { MongoClient } from 'npm:mongodb@6.2.0';
 import type { Blog, BlogPost, Tag } from '../../types/index.ts';
 
-const env = await load();
+await load({ export: true });
 
 // // Local
 // const client = new MongoClient('mongodb://127.0.0.1:27017');
 
 const client = new MongoClient(
-  `mongodb+srv://${env['MONGO_USER']}:${env['MONGO_PW']}@${
-    env['MONGO_DOMAIN']
+  `mongodb+srv://${Deno.env.get('MONGO_USER')}:${Deno.env.get('MONGO_PW')}@${
+    Deno.env.get('MONGO_DOMAIN')
   }/deno?authMechanism=SCRAM-SHA-1`,
 );
 
 await client.connect();
 
-const db = client.db(env['MONGO_DB']);
+const db = client.db(Deno.env.get('MONGO_DB'));
 
 export const blogs = db.collection<Blog>('blogs');
 export const blogPosts = db.collection<BlogPost>('blogPosts');
